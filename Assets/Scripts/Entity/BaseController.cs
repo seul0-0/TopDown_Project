b2a00c6c4cfcca446361d2dbf8bf92bb1a0115f1
xonxseo <jsg8369@naver.com>
2025-07-25 20:38:06 +0900

    Commit4

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -20,10 +20,13 @@ public class BaseController : MonoBehaviour
     
     private Vector2 knockback = Vector2.zero;
     private float knockbackDuration = 0.0f;
+    
+    protected AnimationHandler animationHandler;
 
     protected virtual void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
     }
 
     protected virtual void Start()
@@ -61,6 +64,7 @@ public class BaseController : MonoBehaviour
         }
 
         _rigidbody.velocity = direction;
+        animationHandler.Move(direction);
     }
 
     private void Rotate(Vector2 direction)
--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : BaseController
@@ -11,28 +9,6 @@ public class PlayerController : BaseController
         base.Start();
         camera = Camera.main;
     }
-
-    /*protected override void HandleAction()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2(horizontal, vertical).normalized;
-        
-        Vector2 mousPosition = Input.mousePosition;
-        Vector2 worldPos = camera.ScreenToWorldPoint(mousPosition);
-        lookDirection = (worldPos - (Vector2)transform.position);
-
-        if (lookDirection.magnitude > .9f)
-        {
-            lookDirection = Vector2.zero;
-        }
-        else
-        {
-            lookDirection = lookDirection.normalized;
-        }
-    }*/
-    
-    // Update is called once per frame
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
